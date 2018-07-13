c8e52c161ce6f1abef9779a0a0d3e9ef604e4f4c
vexxoz <blakecaldwell123@gmail.com>
2018-07-12 23:23:48 -0700

    Getting started on PHP

---
+++ PHP Version/SMTimeCalc.php
@@ -0,0 +1,31 @@
+<?php
+
+$numConsultants = 0;
+
+$main = array();
+
+for ($i=0; $i < $numConsultants; $i++) { 
+	array_push($main, new array());
+}
+
+
+/**
+ * 
+ */
+class Consultant
+{
+
+	$people = array();
+	$hairCount = 0;
+	$makeupCount = 0;
+	$serviceTime = 0;
+	$serviceCount = 0;
+
+	function __construct($)
+	{
+		# code...
+	}
+}
+
+
+?>
