03088e26a65db6c16af80eae69a44375bfb91db2
vexxoz <blakecaldwell123@gmail.com>
2018-08-15 17:36:12 -0700

    Added Dragable Clients to each consultant

--- PHP Version/SMTimeCalc.php
+++ PHP Version/SMTimeCalc.php
@@ -2,27 +2,41 @@
 
 $numConsultants = 3;
 
-$makeupCount = 1;
+$makeupCount = 4;
 $hairCount = 1;
 
-$totalServices = $makeupCount+$hairCount;
+$totalServices = floor(($makeupCount+$hairCount+2)/$numConsultants);
 
 $main = array();
 
+$brideDone = false;
+
 for ($i=0; $i < $numConsultants; $i++) { 
 	$main[] = new Consultant();
-	for ($j=0; $j < $totalServices; $j++) { 
-		$main[$i]->addClient();
+	$tempTotalService = $totalServices;
+	while($tempTotalService > 0){
+		if($brideDone == false){
+			$main[$i]->addClient("Bride");
+			$brideDone = true;
+			$tempTotalService = $tempTotalService - 2;
+		}else{
+			if($tempTotalService > 0){
+				$main[$i]->addClient("");	
+			}
+			
+			$tempTotalService = $tempTotalService -1;
+		}
 	}
 }
 
 for($i=0; $i < count($main); $i++){
 	// echo "cycling through consultants</br>";
-	echo "Consultant " . $i . ": </br>";
-	$main[$i]->callClientPrint();
+	$int = $i +1;
+	$inner = $main[$i]->callClientPrint();
+	echo "<h2>Consultant".$int."</h2><div id=\"consultant\"><ul class=\"sortable\">" . $inner . "</ul></div>";
 }
 
-// var_dump($main);
+// var_dump($inner);
 
 /**
  * 
@@ -38,15 +52,16 @@ class Consultant
 		$this->clients = array();
 	}
 
-
-	function addClient(){
-		$this->clients[] = new Client();
+	function addClient($nameIn){
+		$this->clients[] = new Client($nameIn);
 	}
 
 	function callClientPrint(){
+		$output = "";
 		for ($i=0; $i < count($this->clients); $i++) { 
-			$this->clients[$i]->printClient();
+			$output .= $this->clients[$i]->printClient();
 		}
+		return $output;
 	}
 
 }
@@ -57,14 +72,68 @@ class Consultant
 class Client
 {
 	
-	function __construct()
+	private $name;
+
+	function __construct($nameIn)
 	{
-		echo "adding client to consultant</br>";
+		// echo "adding client to consultant</br>";
+		if($nameIn == ""){
+			$this->name = "service";
+		}else{
+			$this->name = $nameIn;	
+		}
 	}
 
 	function printClient(){
-		echo "Client</br>";
+		$output = "<li>" . $this->name . "</li>";
+		return $output;
 	}
 }
 
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Sortable - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  .sortable{
+    min-height: 50px;
+  }
+  #consultant{
+    border-bottom: solid 2px black;
+    width: 100%;
+    min-height: 50px;
+    background: grey;
+  }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+    $(function() {
+        var oldList, newList, item;
+        $('.sortable').sortable({
+            start: function(event, ui) {
+                item = ui.item;
+                newList = oldList = ui.item.parent().parent();
+            },
+            stop: function(event, ui) {          
+                // alert("Moved " + item.text() + " from " + oldList.attr('id') + " to " + newList.attr('id'));
+            },
+            change: function(event, ui) {  
+                if(ui.sender) newList = ui.placeholder.parent().parent();
+            },
+            connectWith: ".sortable"
+        }).disableSelection();
+    });
+  </script>
+</head>
+<body>
+ 
+
+
+</body>
+</html>
