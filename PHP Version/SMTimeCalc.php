e2fe4fe33ea403b3599857636e8aa876beb15eed
Blake Caldwell <blakecaldwell123@gmail.com>
2019-05-20 23:45:48 -0700

    Big Update
    Added buttons for bride, hair, makeup.
    Added timing for hair and makeup with update button.
    Added add consultant button.
    Added auto updating of times

--- PHP Version/SMTimeCalc.php
+++ PHP Version/SMTimeCalc.php
@@ -1,109 +1,16 @@
-<?php
-
-$numConsultants = 3;
-
-$makeupCount = 4;
-$hairCount = 1;
-
-$totalServices = floor(($makeupCount+$hairCount+2)/$numConsultants);
-
-$main = array();
-
-$brideDone = false;
-
-for ($i=0; $i < $numConsultants; $i++) { 
-	$main[] = new Consultant();
-	$tempTotalService = $totalServices;
-	while($tempTotalService > 0){
-		if($brideDone == false){
-			$main[$i]->addClient("Bride");
-			$brideDone = true;
-			$tempTotalService = $tempTotalService - 2;
-		}else{
-			if($tempTotalService > 0){
-				$main[$i]->addClient("");	
-			}
-			
-			$tempTotalService = $tempTotalService -1;
-		}
-	}
-}
-
-for($i=0; $i < count($main); $i++){
-	// echo "cycling through consultants</br>";
-	$int = $i +1;
-	$inner = $main[$i]->callClientPrint();
-	echo "<h2>Consultant".$int."</h2><div id=\"consultant\"><ul class=\"sortable\">" . $inner . "</ul></div>";
-}
-
-// var_dump($inner);
-
-/**
- * 
- */
-class Consultant
-{
-
-	private $clients;
-
-	function __construct()
-	{
-		// echo "adding consultant</br>";
-		$this->clients = array();
-	}
-
-	function addClient($nameIn){
-		$this->clients[] = new Client($nameIn);
-	}
-
-	function callClientPrint(){
-		$output = "";
-		for ($i=0; $i < count($this->clients); $i++) { 
-			$output .= $this->clients[$i]->printClient();
-		}
-		return $output;
-	}
-
-}
-
-/**
- * 
- */
-class Client
-{
-	
-	private $name;
-
-	function __construct($nameIn)
-	{
-		// echo "adding client to consultant</br>";
-		if($nameIn == ""){
-			$this->name = "service";
-		}else{
-			$this->name = $nameIn;	
-		}
-	}
-
-	function printClient(){
-		$output = "<li>" . $this->name . "</li>";
-		return $output;
-	}
-}
-
-?>
 <!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Sortable - Default functionality</title>
+  <title>Salon Maison Schedule Maker</title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
   <style>
   .sortable{
     min-height: 50px;
+	list-style-type: none;
   }
-  #consultant{
+  [id*="consultant_id_"]{
     border-bottom: solid 2px black;
     width: 100%;
     min-height: 50px;
@@ -112,28 +19,172 @@ class Client
   </style>
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+</head>
+<body>
+	<button name="addConst" id="addConst" onclick="addConsultant()">Add Consultant</button>
+	<button name="addBride" id="addBride" onclick="addService('Bride')">Add Bride</button>	
+	<button name="addHair" id="addHair" onclick="addService('Hair')">Add Hair</button>
+	<button name="addMakeup" id="addMakeup" onclick="addService('Makeup')">Add Makeup</button>
+	<button name="export" id="export" onclick="">Export Schedule</button>	
+
+	
+	<div name="results" id="results">
+		
+	</div>
+	<div id="test"></div>
+</body>
   <script>
-    $(function() {
-        var oldList, newList, item;
+  // sortable variables
+    var oldList, newList, item;
+	// count for the consultants
+	var count = 1;
+	// start time
+	var startTime = new Date("27 July 2016 07:00:00");
+	
+	// add a consultant to the results div/schedule
+	function addConsultant(){
+		// checks to see if at least one consultant has been added
+		if(!$("#main").length){
+			document.getElementById("results").innerHTML += "<h2>Consultant "+count+"</h2><input type='text' id='consultant_makeup_"+count+"' value='45' placeholder='Makeup Time'/><input type='text' id='consultant_hair_"+count+"' value='45' placeholder='Hair Time'/><button onclick=\"updateTime('"+count+"')\">Update</button><div id='consultant_id_"+count+"' data-makeup-time='45' data-hair-time='45'><ul class='sortable' id='main'></ul></div>";
+		}else{
+			document.getElementById("results").innerHTML += "<h2>Consultant "+count+"</h2><input type='text' id='consultant_makeup_"+count+"' value='45' placeholder='Makeup Time'/><input type='text' id='consultant_hair_"+count+"' value='45' placeholder='Hair Time'/><button onclick=\"updateTime('"+count+"')\">Update</button><div id='consultant_id_"+count+"' data-makeup-time='45' data-hair-time='45'><ul class='sortable'></ul></div>";
+		}
+		count++;
+		// make new elements sortable
+		newSortable();
+	}
+	
+	// update the data tags for the consultants hair and makeup completion times
+	function updateTime(id){
+		var newHair = document.getElementById("consultant_hair_" + id).value;
+		var newMakeup = document.getElementById("consultant_makeup_" + id).value;
+		document.getElementById("consultant_id_" + id).setAttribute("data-makeup-time", newMakeup);
+		document.getElementById("consultant_id_" + id).setAttribute("data-hair-time", newHair);
+		recountTime("consultant_id_" + id,'');
+	}	
+	
+	// adds a service of a type to the first consultant, can be dragged to other consultants from there
+	function addService(name){
+		if($("#main").length){
+			document.getElementById("main").innerHTML += "<li><span id='service_start'>7:00</span> - <span id='service_end'>7:30</span> "+name+"</li>";
+			recountTime('consultant_id_1', '');
+		}else{
+			alert("You dont have at least 1 consultant to add a service to!");
+		}
+		
+	}
+	
+	// recounts the time line for the consultant the service was pulled from and the consultant the service was given to
+	function recountTime(fromId, toId){
+		// if the from id is set
+		if(fromId != ''){
+			// get the time from start time
+			var time = startTime;
+			//get the consultants hair and makeup times
+			var hairTime = document.getElementById(fromId).getAttribute("data-hair-time");
+			var makeupTime = document.getElementById(fromId).getAttribute("data-makeup-time");
+			
+			// get the div from the consultant
+			var div = document.getElementById(fromId);
+			//get the ul from aka the List of services
+			var lists = div.getElementsByTagName("ul");
+			//loop through all ul (USeless???)
+			for (var i = 0; i < lists.length; ++i) {
+				//loop through services
+		        var items = lists[i].getElementsByTagName("li");
+		        for (var j = 0; j < items.length; ++j) {
+					// get the two spans for start and end time in an array
+					var spans = items[j].getElementsByTagName("span");
+					// if the service is a hair
+					if(items[j].innerHTML.includes("Hair")){
+						// update start time
+						spans[0].innerHTML = time.toLocaleTimeString();
+						// update time
+						time = new Date(time.getTime() + hairTime*60000);
+						// update end time
+						spans[1].innerHTML = time.toLocaleTimeString();
+					}
+					if(items[j].innerHTML.includes("Makeup")){
+						// update start time
+						spans[0].innerHTML = time.toLocaleTimeString();
+						// update time
+						time = new Date(time.getTime() + makeupTime*60000);
+						// update end time
+						spans[1].innerHTML = time.toLocaleTimeString();
+					}
+					if(items[j].innerHTML.includes("Bride")){
+						// update start time
+						spans[0].innerHTML = time.toLocaleTimeString();
+						// update time
+						time = new Date(time.getTime() + 120*60000);
+						// update end time
+						spans[1].innerHTML = time.toLocaleTimeString();
+					}
+		        }
+			}
+		// if the toId is set
+		}if(toId != ''){
+			var time = startTime;
+			var hairTime = document.getElementById(toId).getAttribute("data-hair-time");
+			var makeupTime = document.getElementById(toId).getAttribute("data-makeup-time");
+			
+			var div = document.getElementById(toId);
+			var lists = div.getElementsByTagName("ul");
+			for (var i = 0; i < lists.length; ++i) {
+				
+		        var items = lists[i].getElementsByTagName("li");
+		        for (var j = 0; j < items.length; ++j) {
+					
+					var spans = items[j].getElementsByTagName("span");
+					if(items[j].innerHTML.includes("Hair")){
+						// update start time
+						spans[0].innerHTML = time.toLocaleTimeString();
+						// update time
+						time = new Date(time.getTime() + hairTime*60000);
+						// update end time
+						spans[1].innerHTML = time.toLocaleTimeString();
+					}
+					if(items[j].innerHTML.includes("Makeup")){
+						// update start time
+						spans[0].innerHTML = time.toLocaleTimeString();
+						// update time
+						time = new Date(time.getTime() + makeupTime*60000);
+						// update end time
+						spans[1].innerHTML = time.toLocaleTimeString();
+					}
+					if(items[j].innerHTML.includes("Bride")){
+						// update start time
+						spans[0].innerHTML = time.toLocaleTimeString();
+						// update time
+						time = new Date(time.getTime() + 120*60000);
+						// update end time
+						spans[1].innerHTML = time.toLocaleTimeString();
+					}
+		        }
+			}
+		}
+	}
+	
+	// calls sortable for any existing consultants on load
+	newSortable();
+	
+	// function makes all elements in results as a sortable so the services can be dragged around
+	function newSortable(){
         $('.sortable').sortable({
             start: function(event, ui) {
                 item = ui.item;
                 newList = oldList = ui.item.parent().parent();
             },
             stop: function(event, ui) {          
-                // alert("Moved " + item.text() + " from " + oldList.attr('id') + " to " + newList.attr('id'));
+                 //alert("Moved " + item.text() + " from " + oldList.attr('id') + " to " + newList.attr('id'));
+				 recountTime(oldList.attr('id'), newList.attr('id'));
             },
             change: function(event, ui) {  
                 if(ui.sender) newList = ui.placeholder.parent().parent();
             },
             connectWith: ".sortable"
-        }).disableSelection();
-    });
-  </script>
-</head>
-<body>
- 
-
-
-</body>
+			}).disableSelection();
+	}
+		
+	</script>
 </html>
